4751158aed986b0a74cdc293c3943fe62ff89e0e
NicolRivera0103 <nicolf.riverad@uqvirtual.edu.co>
2026-06-01 11:51:03 -0500

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -1,21 +1,16 @@
-// Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
+import { initializeApp } from "https://www.gstatic.com/firebasejs/12.1.0/firebase-app.js";
+import { getFirestore } from "https://www.gstatic.com/firebasejs/12.1.0/firebase-firestore.js";
 
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
   apiKey: "AIzaSyD22s6E5un2RCPPbi7VouTUqfJDFxC54ak",
   authDomain: "casillerosuniversidad.firebaseapp.com",
   projectId: "casillerosuniversidad",
   storageBucket: "casillerosuniversidad.firebasestorage.app",
   messagingSenderId: "664244381575",
-  appId: "1:664244381575:web:41769884301051f08cd522",
-  measurementId: "G-ECWELGZ86T"
+  appId: "1:664244381575:web:41769884301051f08cd522"
 };
 
-// Initialize Firebase
 const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
+const db = getFirestore(app);
+
+export { db };
